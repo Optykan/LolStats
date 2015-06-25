24933cd8faf26c5fe0af99c10fe2253b23f9c2e5
Optykan <syang0525@gmail.com>
2015-06-25 17:29:09 -0400

    these commites arent helpful

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         
         <?php
 
-            $myfile = fopen("api.key", "r") or die("404 File Not Found");
+            //$myfile = fopen("api.key", "r") or die("404 File Not Found");
             $name = $_POST['inputname'];
             $jsonurl = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+$name+"?api_key=b0cc9773-08ca-4a5b-8d05-f767de88fcc3";
             $json = file_get_contents($jsonurl);

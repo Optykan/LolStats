529d90cefa565839a4dc7018f7251dfd8ce29399
Optykan <syang0525@gmail.com>
2015-06-25 17:21:53 -0400

    what is happening

--- index.php
+++ index.php
@@ -16,12 +16,12 @@
         
         <?php
 
-            $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+            $myfile = fopen("api.key", "r") or die("404 File Not Found");
             $name = $_POST['inputname'];
             $jsonurl = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+$name+"?api_key=b0cc9773-08ca-4a5b-8d05-f767de88fcc3";
             $json = file_get_contents($jsonurl);
             var_dump(json_decode($json));
-        
+            echo "<p id='idresult'>$json</p>";
         ?>
 
 
@@ -31,7 +31,6 @@
         <input type="text" name="inputname">
         <button onclick="request()">Submit</button>
         
-        <p name="idresult"></p>
         
 <!--        <script>
             function request(){

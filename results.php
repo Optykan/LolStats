c08471ab2bceef2914313c40932b36018185a296
Optykan <syang0525@gmail.com>
2015-06-25 21:19:26 -0400

    sorry!

--- results.php
+++ results.php
@@ -11,13 +11,14 @@
             $id = $data[$name]['id'];
             $level = $data[$name]['summonerLevel'];
             if(isset($id)){
-                echo "Summoner not found";   
-            }
-            else{
-            echo "<body>".$id."</br>";
+                 echo "<body>".$id."</br>";
             echo $level;
             echo "<script> console.log($id)</script>";
             echo "<script> console.log($level)</script></body>";
+            }
+            else{
+                echo "Summoner not found";   
+           
             }
         ?>
     </head>
